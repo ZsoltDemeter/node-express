b5556f3db67abea8d1c97e364422cf6ba1c44f9a
ZsoltDemeter <zsoltd.com@gmail.com>
2023-12-29 14:39:12 +0200

    some headers added

--- OFP-api/app.js
+++ OFP-api/app.js
@@ -19,7 +19,8 @@ connectToDb((err) => {
 // CORS middleware to allow requests from a specific origin
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://ofp-admin-panel.netlify.app');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
 });
 
